5a698431aa09d1df4178668f9dd0c62954d7d403
Lucas Cheung <>
2021-06-17 10:29:50 +0100

    added axios for fetching the properties from api

--- src/components/PropertyListing/PropertyListing.js
+++ src/components/PropertyListing/PropertyListing.js
@@ -1,30 +1,54 @@
-import React from 'react';
-import PropertyCard from '../PropertyCard';
-import './PropertyListing.scss';
+import React, { useEffect, useState } from "react";
+import PropertyCard from "../PropertyCard";
+import "./PropertyListing.scss";
+import axios from "axios";
 
-const DUMMY_PROPERTY = {
-    id: 73864112,
-    bedrooms: 3,
-    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
-    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
-    propertyType: 'Flat',
-    price: 1950000,
-    branchName: 'M2 Property, London',
-    propertyUrl: '/property-for-sale/property-73864112.html',
-    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
-    propertyTitle: '3 bedroom flat for sale',
-    mainImage: 'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg'
-};
+// const DUMMY_PROPERTY = {
+//   id: 73864112,
+//   bedrooms: 3,
+//   summary:
+//     "Property 1 Situated moments from the River Thames in Old Chelsea...",
+//   displayAddress: "1 CHEYNE WALK, CHELSEA, SW3",
+//   propertyType: "Flat",
+//   price: 1950000,
+//   branchName: "M2 Property, London",
+//   propertyUrl: "/property-for-sale/property-73864112.html",
+//   contactUrl: "/property-for-sale/contactBranch.html?propertyId=73864112",
+//   propertyTitle: "3 bedroom flat for sale",
+//   mainImage:
+//     "https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg",
+// };
 
 const PropertyListing = () => {
-    return (
-        <div className="PropertyListing">
-            {
-                Array(5).fill(DUMMY_PROPERTY)
-                    .map((property, index) => <PropertyCard key={index} {...property}/>)
-            }
-        </div>
-    )
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    try {
+      const fetchProperties = async () => {
+        const response = await axios.get("/api/properties");
+        setProperties(response.data);
+        setLoading(false);
+      };
+      fetchProperties();
+    } catch (error) {
+      setError(error);
+    }
+  }, []);
+
+  return (
+    <div className="PropertyListing">
+      {loading && <div>Loading</div>}
+      {error && <div>{error}</div>}
+      {!loading &&
+        !error &&
+        properties &&
+        Array(5)
+          .fill(properties)
+          .map((property, index) => <PropertyCard key={index} {...property} />)}
+    </div>
+  );
 };
 
 export default PropertyListing;
